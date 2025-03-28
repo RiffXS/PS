bb206eafa0b72026153d155991b963144a8bbb0c
Rafael <RiffXS>
2025-03-27 21:14:22 -0300

    Remove one calculator, renaming other calculator

--- 1 - Calculadora (basica)/src/br/ifes/projetosistemas/calculadora/model/operation/Dividir.java
+++
@@ -1,9 +0,0 @@
-package br.ifes.projetosistemas.calculadora.model.operation;
-
-public class Dividir implements IOperation{
-
-    public int calc (int value1, int value2){
-        if (value2 == 0)  throw new ArithmeticException("xxxx");
-        return value1 / value2;
-    }
-}
--- 1 - Calculadora (basica)/src/br/ifes/projetosistemas/calculadora/model/operation/IOperation.java
+++
@@ -1,6 +0,0 @@
-package br.ifes.projetosistemas.calculadora.model.operation;
-
-public interface IOperation {
-
-    public int calc (int value1, int value2);
-}
